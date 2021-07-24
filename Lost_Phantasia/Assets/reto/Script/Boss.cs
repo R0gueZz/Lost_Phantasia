e64bacff449a562e037c62bab88034269b413e10
retotototo <retobotkon@gmail.com>
2021-07-24 15:55:59 +0900

    seirisimasita
    seirisimasita

--- Lost_Phantasia/Assets/reto/Script/Boss.cs
+++ Lost_Phantasia/Assets/reto/Script/Boss.cs
@@ -1,253 +1,257 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class Boss : EnemyBase
-//{
-//    enum ActionPattern
-//    {
-//        stay,
-//        Move,
-//        bodyBlow,
-//        jumpAttack,
-//        Dead
-//    }
-//    Rigidbody rb;
-  
+public class Boss : EnemyBase
+{
+    enum BossActionPattern
+    {
+        Stay,
+        Move,
+        BodyBlow,
+        JumpAttack,
+        Dead
+    }
+
+    Rigidbody rb;
+    Animator animator;
 
 
-//    WaitForSeconds actionSetWait = new WaitForSeconds(1f);
-//    WaitForSeconds attackWait = new WaitForSeconds(1.7f);
+    GameObject Player;
+    [SerializeField]
+    GameObject shockWave;
+    [SerializeField]
+    GameObject BodyBlowDecision;
 
-//    int state = 0;
+    [SerializeField]
+    float movetime;
 
-//    int stateRandom = 0;
+    int state = 0;
 
-//    Animator animator;
+    Ray Groundray;
 
-//    GameObject player;
-//    testDamege testDamege;
+    int layer;
 
-//    //攻撃をわかりやすくするための色変更用
-//    Renderer renderer;
-//    bool isBodyBlow = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+        layer = LayerMask.NameToLayer("Ground");
+        StartCoroutine(ActionSet());
+    }
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        renderer = GetComponent<Renderer>();
-//        animator = GetComponent<Animator>();
-//        rb = GetComponent<Rigidbody>();
-//        player = GameObject.FindGameObjectWithTag("Player");
-//        testDamege = player.GetComponent<testDamege>();
-
-//        //StartCoroutine(ActionSet());
-//        StartCoroutine(Move());
-//    }
-
-//    private void Update()
-//    {
+    float tmp;
+    void Update()
+    {
         
-//    }
+    }
+    private void OnGUI()
+    {
+        GUILayout.Label($"HP : {Hp}");
+        GUILayout.Label($"{(BossActionPattern)state}");
+    }
 
+    #region BossAction
+    IEnumerator ActionSet()
+    {
+        if(!ismove)
+        {
+            yield break;
+        }
+        if (Vector3.Distance(transform.position,Player.transform.position) > 8)
+        {
+            StartCoroutine(Move());
+            Debug.Log(Vector3.Distance(transform.position, Player.transform.position) );
+            yield break;
+        }
+        tmp = Random.RandomRange(0, 10001) % 100;
+        if (0 <= tmp && tmp < 20)
+        {   
+            StartCoroutine(Move());
+        }
+        else if (20 <= tmp && tmp < 60)
+        {
+            state = (int)BossActionPattern.Stay;
+            animator.SetInteger("State", state);
+            yield return new WaitForSeconds(0.5f);
 
+            StartCoroutine(JumpAttack());
+        }
+        else if (60 <= tmp && tmp < 100)
+        {
+            state = (int)BossActionPattern.Stay;
+            animator.SetInteger("State", state);
+            yield return new WaitForSeconds(0.5f);
 
-//    //行動を決める
-//    IEnumerator ActionSet ()
-//    {
-//        //IsMoveがfalseの時動かない
-//        yield return new WaitUntil(()=> IsMove);
-//        //一旦待機状態にする
-//        state = 0;
-//        animator.SetInteger("State", 0);
-//        renderer.material.color = Color.black;
-//        //待つ
-//        yield return actionSetWait;
-//        if (transform.position.x - player.transform.position.x >= 5 || transform.position.x - player.transform.position.x <= -5)
-//        {
-//            StartCoroutine(Move());
-//            yield break;
-//        }
+            StartCoroutine(BodyBlow());
+        }
+        yield break;
+    }
 
-//        //ランダムで行動を決める
-//        stateRandom =  Random.Range(1, 100);
-//        //行動を行う
-//        if (0 <= stateRandom && stateRandom < 10)
-//        {
-//            StartCoroutine(Stay());
+    float moveStopDistance = 1;
+    float motionTime = 0;
+    IEnumerator Move()
+    {
+        if (!ismove)
+        {
+            yield break;
+        }
+        motionTime = 0;
+       
+        state = (int)BossActionPattern.Move;
+        animator.SetInteger("State", state);
+
+        while (motionTime < movetime && Vector3.Distance(transform.position, Player.transform.position) > 1.5f)
+        {
             
-//        }
-//        if(10 <= stateRandom && stateRandom < 20)
-//        {
-//            StartCoroutine(Move());
-//        }
-//        else if(20 <= stateRandom && stateRandom < 60 )
-//        {
-//            StartCoroutine(BodyBlow());
-            
-//        }
-//        else if(60 <= stateRandom && stateRandom <= 100)
-//        {
-//            StartCoroutine(JumpAtteck());
-            
-//        }
-//        //アニメーターに状態を渡す
-//        animator.SetInteger("State", state);
-//        yield break;
+            if (!ismove)
+            {
+                yield break;
+            }
 
-//    }
+            rb.AddForce(-(transform.position.x - Player.transform.position.x), 0, 0);
+            rb.velocity = new Vector3(Mathf.Clamp(rb.velocity.x, -2, 2), rb.velocity.y, rb.velocity.z);
+            motionTime += Time.deltaTime;
+            LookPlayer();
+            yield return null;
 
-//    public override void Dead()
-//    {
-//        renderer.material.color = Color.black;
-//        state = (int)ActionPattern.Dead;
-//        animator.SetInteger("State", state);
-//        Invoke("InvokeBaseDead", 1);
-//    }
+        }
 
-//    void InvokeBaseDead ()
-//    {
-//        base.Dead();
-//    }
+        state = (int)BossActionPattern.Stay;
+        animator.SetInteger("State", state);
+        yield return null;
+        StartCoroutine(ActionSet());
+        yield break;
+    }
 
+    WaitForSeconds jumpWait = new WaitForSeconds(2f);
+    WaitForSeconds shockWaveTime = new WaitForSeconds(1f);
+    RaycastHit raycastHit;
+    IEnumerator JumpAttack()
+    {
+        if (!ismove)
+        {
+            yield break;
+        }
+       
+        state = (int)BossActionPattern.JumpAttack;
+        animator.SetInteger("State", state);
+        rb.AddForce(0, 10, 0, ForceMode.Impulse);
+        yield return jumpWait;
+        LookPlayer();
+        if (!ismove)
+        {
+            yield break;
+        }
 
-//    void PlayerLook()
-//    {
-//        if (transform.position.x > player.transform.position.x)
-//        {
-//            transform.rotation = Quaternion.Euler(0, 0, 0);
-//        }
-//        else
-//        {
-//            transform.rotation = Quaternion.Euler(0, 180, 0);
-//        }
-//    }
+        yield return new WaitUntil(() => Physics.Raycast(transform.position, -transform.up, out raycastHit, 1.1f) && !raycastHit.collider.CompareTag("Ground"));
 
+        if (!ismove)
+        {
+            yield break;
+        }
+        shockWave.SetActive(true);
+        yield return shockWaveTime;
 
-
-
-//    #region BossAction
-//    IEnumerator Stay ()
-//    {
-//        state = (int)ActionPattern.stay;
-//        renderer.material.color = Color.black;
-//        yield return new WaitForSeconds(0.5f);
-//        PlayerLook();
-//        StartCoroutine(ActionSet());
-//        yield break;
-//    }
-//    IEnumerator Move ()
-//    {
-//        PlayerLook();
-//        state = (int)ActionPattern.Move;
-//        renderer.material.color = Color.green;
-//        for(int i = 0; i <= 1500 && (transform.position.x - player.transform.position.x >= 2 || transform.position.x - player.transform.position.x <= -2); i++)
-//        {
-
-//            rb.constraints = (RigidbodyConstraints)124;
-//            rb.velocity =  new Vector3(Mathf.Clamp(rb.velocity.x +transform.position.x - player.transform.position.x,-1,1), 0, 0).normalized ;
-//            yield return null;
-            
-//            if(i % 20 == 0)
-//            {
-//                PlayerLook();
-//            }
-//        }
-//        PlayerLook();
-//        StartCoroutine(ActionSet());
-//        rb.constraints = RigidbodyConstraints.FreezeAll;
-//        yield break;
-//    }
-
-//    IEnumerator BodyBlow ()
-//    {
-//        state = (int)ActionPattern.bodyBlow;
-//        rb.constraints = (RigidbodyConstraints)124;
-//        renderer.material.color = Color.blue;
-//        isBodyBlow = true;
-//        Invoke("BodyBlowStop",0.15f);
-//        yield return attackWait;
-//        PlayerLook();
-//        StartCoroutine(ActionSet());
-//        yield break;
-//    }
-//    IEnumerator JumpAtteck ()
-//    {
-//        state = (int)ActionPattern.jumpAttack;
-//        renderer.material.color = Color.red;
-//        rb.constraints = (RigidbodyConstraints)122;
-//        StartCoroutine(JumpAttackDamege());
-//        yield return attackWait;
-//        PlayerLook();
-//        StartCoroutine(ActionSet());
-//        rb.constraints = (RigidbodyConstraints)126;
-
-//        yield break;
-//    }
-
-
-//    private void OnDrawGizmosSelected()
-//    {
-//        Gizmos.color = Color.red;
-//        Gizmos.DrawWireCube(transform.position - new Vector3(0f, 0.5f, 0f), new Vector3(6, 1, 2));
-
-//    }
-
-
-//    bool ishit = false;
-//    RaycastHit[] hit;
-//    int attackframe = 0;
-//    IEnumerator JumpAttackDamege()
-//    {
-//        ishit = false;   
-//        yield return new WaitForSeconds(1.5f);
-//        for(attackframe = 0; attackframe < 200;attackframe++)
-//        {
-           
-//            Debug.Log("aa");
-//            hit = Physics.BoxCastAll(transform.position - new Vector3(0f, 0.5f, 0f), new Vector3(3, 1, 2), -transform.up, Quaternion.identity, LayerMask.GetMask("Player"));
-//            foreach(var i in hit)
-//            {
-//                if(i.collider.gameObject.CompareTag("Player"))
-//                {
-//                    testDamege.Damege();
-//                    yield break;
-//                }
-//            }
-            
-//            attackframe++;
-//            yield return null;
-//        }
+        if (!ismove)
+        {
+            yield break;
+        }
+        shockWave.SetActive(false);
+        state = (int)BossActionPattern.Stay;
+        animator.SetInteger("State", state);
+        yield return new WaitForSeconds(0.5f);
+        LookPlayer();
+        if (!ismove)
+        {
+            yield break;
+        }
         
-//        yield break;
+        StartCoroutine(ActionSet());
+        yield break;
+    }
+
+    IEnumerator BodyBlow()
+    {
+        LookPlayer();
+        if (!ismove)
+        {
+            yield break;
+        }
+        state = (int)BossActionPattern.BodyBlow;
+        animator.SetInteger("State", state);
+        yield return new WaitForSeconds(1f);
+
+        if (!ismove)
+        {
+            yield break;
+        }
+        rb.AddForce(transform.right * 7, ForceMode.Impulse);
+        BodyBlowDecision.SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        if (!ismove)
+        {
+            yield break;
+        }
+        BodyBlowDecision.SetActive(false);
+        state = (int)BossActionPattern.Stay;
+        animator.SetInteger("State", state);
+        yield return new WaitForSeconds(0.5f);
+
+        if (!ismove)
+        {
+            yield break;
+        }
         
-//    }
-//    void BodyBlowStop ()
-//    {
-//        isBodyBlow = false;
-//    }
-//    #endregion
-//    private void OnCollisionEnter(Collision collision)
-//    {
-//          //タックル中だけ変える
-//        if(isBodyBlow && collision.gameObject.CompareTag("Player"))
-//        {
-//            Debug.Log("BodyBlow");
-//            testDamege.Damege();
-//        }
-//        //タックル中ではないときの接触ダメージ
-//        if(collision.gameObject.CompareTag("Player"))
-//        {
-//            testDamege.Damege();
-//        }
-//    }
+        StartCoroutine(ActionSet());
+        yield break;
+        LookPlayer();
+
+    }
+
+    override public void Dead ()
+    {
+        state = (int)BossActionPattern.Dead;
+        animator.SetInteger("State", state);
+        rb.velocity = Vector3.zero;
+        rb.constraints = RigidbodyConstraints.FreezeAll;
+        shockWave.SetActive(false);
+        BodyBlowDecision.SetActive(false);
+        Invoke("InvokeBaseDead", 1f);
+    }
+
+    void InvokeBaseDead()
+    {
+        base.Dead();
+    }
 
 
-//    //ボスの体力確認用
-//    private void OnGUI()
-//    {
-//        GUILayout.Label($"BossHP : {Hp}");
-//        GUILayout.Label($"State : {(ActionPattern)state}");
-//    }
+    void LookPlayer()
+    {
+        Debug.Log(transform.position.x - Player.transform.position.x);
+        transform.LookAt(Player.transform.position);
+        transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y - 90, 0);
+        
+    }
+    #endregion
 
-//}
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            if(state == (int)BossActionPattern.BodyBlow)
+            {
+                Debug.Log("BodyBlowHit");
+            }
+            if(state == (int)BossActionPattern.JumpAttack)
+            {
+                Debug.Log("JumpAttackHit");
+            }
+            
+        }
+    }
+
+}
